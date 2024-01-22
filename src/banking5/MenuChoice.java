b0b14aa329737fd2cdc8384afc46cc72387861be
junulee.dk <junulee.dk@gmail.com>
2024-01-22 15:38:21 +0900

    질문

---
+++ src/banking5/MenuChoice.java
@@ -0,0 +1,60 @@
+package banking5;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuChoice implements IcustomDefine {
+
+	public void showMenu() {
+
+		System.out.println("### Menu ###");
+		System.out.print("1.계좌개설      ");
+		System.out.println("2.입  금");
+		System.out.print("3.출  금");
+		System.out.println("       4.계좌정보출력");
+		System.out.print("5.계정정보삭제 ");
+		System.out.println("  6.프로그램 종료");
+		System.out.print("선택: ");
+	}
+	
+	public void menuChoice() {
+
+		Scanner input = new Scanner(System.in);
+		AccountManager accM = new AccountManager();
+		
+		while (true) {
+			try {
+			showMenu();
+			int choice = input.nextInt();
+		
+			switch (choice) {
+			case MAKE:
+				accM.makeAccount();
+				break;
+			case DEPOSIT:
+				accM.depositMoney(accM.getChoice(), accM.getAddInterest());
+				break;
+			case WITHDRAW:
+				accM.withdrawMoney();
+				break;
+			case INQUIRE:
+				accM.showAccInfo();
+				break;
+			case DELETE:
+				accM.deleteAccountInfo();
+				break;
+			case EXIT:
+				accM.saveFileExit();
+				System.out.println("프로그램종료");
+				
+				return;
+			default:
+			}
+			}
+			catch (InputMismatchException e) {
+				System.out.println("숫자를 입력해야합니다.");
+				input.nextLine();
+			}
+		}
+	}
+}
---
+++ src/banking5/NormalAccount.java
@@ -0,0 +1,23 @@
+package banking5;
+
+import java.util.Iterator;
+
+class NormalAccount extends Account {
+
+	double interest;
+
+	public NormalAccount(String accountNum, String userName, int balance, double interest) {
+		super(accountNum, userName, balance);
+		this.interest = interest;
+	}
+	
+	@Override
+	 public String toString() {
+		return "계좌번호: " + accountNum + "\n" + "고객이름: " + userName + "\n" + 
+				"잔고: " + balance + "\n"  + "기본이자: " + interest + "%" + "\n" + "----------------";
+	}
+	
+	 // 이자 계산 메서드 추가
+
+}
+
